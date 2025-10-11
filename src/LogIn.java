3495ba25db2a1c8af2febae805afe7c92dcafd09
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-10-12 01:38:20 +0200

    Added Login logic

---
+++ src/LogIn.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LogIn {
+    public static boolean check(Connection conn,String name,String password){
+        try{
+            PreparedStatement stm1=conn.prepareStatement("SELECT * FROM  clients where name=? and password =?");
+            String hashedPassword=PasswordUtil.hashPassword(password);
+            stm1.setString(1,name);
+            stm1.setString(2,hashedPassword);
+            ResultSet rs= stm1.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            } else {
+
+                System.out.println("No rows found.");
+                return false;
+            }
+
+        }
+        catch(SQLException e){
+            System.out.println(e);
+            return false;
+        }
+
+    }
+}
