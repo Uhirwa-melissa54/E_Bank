d3c85785e87dc680531150d82a2493126884d0ec
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-10-12 01:20:09 +0200

    Added signing in

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.sql.Connection;
 public class Main{
     public static void main(String[] args){
         Connection conn=Db.dbConnect();
+        int returnedRow=SignUp.create(conn,"Bonnette Umurerwa","umubonnie@gmail.com","Kigali","What54.");
+
+
+
 
     }
 }
--- src/SignUp.java
+++ src/SignUp.java
@@ -1,17 +1,27 @@
-/*
+
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class SignUp{
-public static void create(Connection conn){
+public static int create(Connection conn, String name, String email, String location, String password){
     try {
-        PreparedStatement stm1 = conn.prepareStatement("INSERT INTO clients (names,email,location,password) values(?,?,?,?)");
+        PreparedStatement stm1 = conn.prepareStatement("INSERT INTO clients (name,email,location,password) values(?,?,?,?)");
+        String hashedPassword = PasswordUtil.hashPassword(password);
+        stm1.setString(1,name);
+        stm1.setString(2,email);
+        stm1.setString(3,location);
+        stm1.setString(4,hashedPassword);
+
+        int rows= stm1.executeUpdate();
+        return rows;
     }
     catch(SQLException e){
         System.out.println(e);
+        return -1;
     }
 
 }
 
-}*/
+}
