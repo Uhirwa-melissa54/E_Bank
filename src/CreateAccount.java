041ea42aab560b1df6535c5b5f17e6e898825b14
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-10-16 00:34:08 +0200

    added creation of accounts

--- src/CreateAccount.java
+++ src/CreateAccount.java
@@ -1,4 +1,23 @@
-public class CreateAccount {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-    
+public class CreateAccount {
+    public  static int createAccount(Connection conn, int id, String name, String accoutName, String accountType, int balance){
+        try {
+            PreparedStatement stm1 = conn.prepareStatement("INSERT INTO accounts(id,nameOfOwner,accountName,accountType,balance) values(?,?,?,?,?)");
+            stm1.setInt(1,id);
+            stm1.setString(2,name);
+            stm1.setString(3,accoutName);
+            stm1.setString(4,accountType);
+            stm1.setInt(5,balance);
+            int rows=stm1.executeUpdate();
+            return rows;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 0;
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 public class Main{
     public static void main(String[] args){
         Connection conn=Db.dbConnectClients();
+        CreateAccount.createAccount(conn,1,"Melissa","account1","SavingAccount",5000);
 
     }
 }
